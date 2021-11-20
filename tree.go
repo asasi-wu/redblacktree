6ab167f567b568d26fcd4e72ceb335275c5862c5
jindongwu <wujindongasasi@gmail.com>
2021-11-20 15:33:40 +0100

    add redblacktreeaddingnode

--- tree.go
+++ tree.go
@@ -13,6 +13,12 @@ type Node struct{
 
 	color bool
 }
+func (n *Node) IsRed() bool {
+	if n == nil {
+		return BLACK
+	}
+	return n.color
+}
 type RedBlackTree struct{
 	size int
 	root *Node
@@ -54,4 +60,47 @@ func (n *Node) changeColors() {
 	n.color = RED
 	n.left.color = BLACK
 	n.right.color = BLACK
+}
+func (tree *RedBlackTree) Add(key, val int)  {
+	isAdd, nd := tree.root.add(key, val)
+	tree.size += isAdd
+	tree.root = nd
+	tree.root.color = BLACK
+}
+
+func (n *Node)add(key,val int)(int, *Node){
+	if n==nil{
+		return 1, NewNode(key,val)
+	}
+	Added:=0
+	if key<n.key{
+		Added,n.left=n.left.add(key,val)
+	}else if key > n.key{
+		Added,n.right=n.right.add(key,val)
+	}else{
+		n.value=val
+	}
+	n=n.updateRedBlackTree(Added)
+	return Added,n
+}
+
+
+
+
+func (n *Node) updateRedBlackTree(Added int) *Node {
+	if Added == 0 {
+		return n
+	}
+	if n.right.IsRed() == RED && n.left.IsRed() != RED {
+		n = n.leftRotate()
+	}
+
+	if n.left.IsRed() == RED && n.left.left.IsRed() == RED {
+		n = n.rightRotate()
+	}
+	if n.left.IsRed() == RED && n.right.IsRed() == RED {
+		n.changeColors()
+	}
+
+	return n
 }
